d3835e140eb64eeeab197196822a1be7cd706972
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2024-03-27 16:52:39 -0400

    AuthorDTO

--- src/main/java/com/lucasywamoto/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/lucasywamoto/springbootmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.lucasywamoto.springbootmongodb.domain.Post;
 import com.lucasywamoto.springbootmongodb.domain.User;
+import com.lucasywamoto.springbootmongodb.dto.AuthorDTO;
 import com.lucasywamoto.springbootmongodb.repositories.PostRepository;
 import com.lucasywamoto.springbootmongodb.repositories.UserRepository;
 
@@ -35,19 +36,20 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 		Post post1 = new Post(null,
 				sdf.parse("21/03/2018"),
 				"Partiu viagem",
 				"Vou viajar para São Paulo. Abraços!",
-				maria);
+				new AuthorDTO(maria));
 		
 		Post post2 = new Post(null,
 				sdf.parse("23/03/2018"),
 				"Bom dia",
 				"Acordei feliz hoje!!",
-				maria);
+				new AuthorDTO(maria));
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));		
 	}
 
