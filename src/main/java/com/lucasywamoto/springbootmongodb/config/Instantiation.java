56dd2fc7818dc922d6bcabcb587ba44fb061624b
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2024-03-27 17:18:21 -0400

    Adding comments to posts

--- src/main/java/com/lucasywamoto/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/lucasywamoto/springbootmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lucasywamoto.springbootmongodb.domain.Post;
 import com.lucasywamoto.springbootmongodb.domain.User;
 import com.lucasywamoto.springbootmongodb.dto.AuthorDTO;
+import com.lucasywamoto.springbootmongodb.dto.CommentDTO;
 import com.lucasywamoto.springbootmongodb.repositories.PostRepository;
 import com.lucasywamoto.springbootmongodb.repositories.UserRepository;
 
@@ -49,6 +50,13 @@ public class Instantiation implements CommandLineRunner{
 				"Bom dia",
 				"Acordei feliz hoje!!",
 				new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/lucasywamoto/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/lucasywamoto/springbootmongodb/resources/PostResource.java
@@ -1,24 +1,13 @@
 package com.lucasywamoto.springbootmongodb.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasywamoto.springbootmongodb.domain.Post;
-import com.lucasywamoto.springbootmongodb.domain.User;
-import com.lucasywamoto.springbootmongodb.dto.UserDTO;
 import com.lucasywamoto.springbootmongodb.services.PostService;
 
 @RestController
